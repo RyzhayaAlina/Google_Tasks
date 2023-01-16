554fd23e129c5a5e5cf9fa498c92a6bc3a8c41aa
User <alina>
2023-01-16 13:41:54 +1000

    Создана разметка главного экрана

--- app/src/main/java/com/example/google_tasks/MainActivity.kt
+++ app/src/main/java/com/example/google_tasks/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.google_tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.google_tasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
