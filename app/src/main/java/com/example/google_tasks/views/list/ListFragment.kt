c7ae9bbbb82e6c8cd97fc727ee56a126117b1549
User <alina>
2023-01-17 18:28:06 +1000

    создан нижний диалог для создания задач

--- app/src/main/java/com/example/google_tasks/views/list/ListFragment.kt
+++ app/src/main/java/com/example/google_tasks/views/list/ListFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.google_tasks.R
+import com.example.google_tasks.databinding.CreateTaskBottomDialogBinding
 import com.example.google_tasks.databinding.FragmentListBinding
 import com.example.google_tasks.models.task.Task
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class ListFragment : Fragment() {
 
     private lateinit var binding: FragmentListBinding
     private val viewModel: ListViewModel by activityViewModels()
     private val adapter = ListAdapter()
+    private lateinit var createTaskBottomDialog: BottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,11 +28,53 @@ class ListFragment : Fragment() {
     ): View {
         binding = FragmentListBinding.inflate(inflater, container, false)
 
+
+        createTaskBottomDialog = BottomSheetDialog(requireContext())
+        createTaskBottomDialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        createBottomDialog()
+
         binding.createTaskFloatingButton.setOnClickListener {
-            viewModel.createTask(Task(name = "task", additInfo = "addit info", isChosen = false))
+            createTaskBottomDialog.show()
         }
 
         return binding.root
+    }
+
+    private fun createBottomDialog() {
+        val dialogBinding = CreateTaskBottomDialogBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+
+        createTaskBottomDialog.setContentView(dialogBinding.root)
+
+        createTaskBottomDialog.setOnDismissListener {
+            dialogBinding.taskNameEditText.text.clear()
+            dialogBinding.taskAdditEditText.text.clear()
+            dialogBinding.taskAdditEditText.visibility = View.GONE
+            dialogBinding.addToChosenCheckBox.isChecked = false
+        }
+
+        dialogBinding.taskNameEditText.requestFocus()
+
+        dialogBinding.showAdditImageButton.setOnClickListener {
+            dialogBinding.taskAdditEditText.visibility = View.VISIBLE
+        }
+
+        dialogBinding.addToChosenCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            dialogBinding.addToChosenCheckBox.setButtonDrawable(
+                if (isChecked) R.drawable.ic_chosen_full else R.drawable.ic_chosen_border
+            )
+        }
+
+        dialogBinding.saveButton.setOnClickListener {
+            val task = Task(
+                name = dialogBinding.taskNameEditText.text.toString(),
+                additInfo = dialogBinding.taskAdditEditText.text.toString(),
+                isChosen = dialogBinding.addToChosenCheckBox.isChecked
+            )
+            viewModel.createTask(task)
+            createTaskBottomDialog.dismiss()
+        }
+
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
